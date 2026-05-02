03f458aa0a3827a9da24a7006732a34227aace83
AnahiMolina <anahi_molina11@outlook.com>
2026-05-02 14:43:00 -0600

    archivos

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './pages/App.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(  
+  <BrowserRouter basename='/dashboard/'>
+    <Routes>
+      <Route path='/' element={<App />}/>
+    </Routes>
+  </BrowserRouter>,
+)
